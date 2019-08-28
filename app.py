44a40add8c264d3d7f8b5da353a1f74f7dbdc217
hsyun89 <hsyun89@naver.com>
2019-08-28 16:23:38 +0900

    heroku setting

--- app.py
+++ app.py
@@ -55,4 +55,6 @@ def telegram():
     return '', 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port=int(os.getenv('PORT',5000))
+    app.run(host='0,0,0,0', port=port,debug=True)
